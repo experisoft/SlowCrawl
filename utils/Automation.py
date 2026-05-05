e14a9341b9aa52185e3f3a0f3c24d25778564da1
aap9002 <aap9002@gmail.com>
2026-05-05 22:37:44 +0100

    Enhance logging in Automation class for better error tracking during scraping

--- utils/Automation.py
+++ utils/Automation.py
@@ -1,5 +1,10 @@
+import time
+import random
+import logging
 from utils.Scraper import Scraper
 from utils.Exporter import Exporter
+
+logging.basicConfig(level=logging.INFO)
 
 class Automation:
     def get_list_of_weblinks_for_scraping(self, file_path: str) -> list:
@@ -11,8 +16,17 @@ class Automation:
         
         for link in list_of_links:
             print(f"Processing link: {link}")
-            scraper = Scraper()
-            scraper.screenshot_active_vouchers(link)
+            try:
+                scraper = Scraper()
+                scraper.screenshot_active_vouchers(link)
+                logging.info(f"Finished processing link: {link}")
+            except Exception as e:
+                logging.error(f"Error processing link {link}: {e}")
+                
+            # sleep_variance = 15
+            # sleep_time = random.uniform(1, 3) + sleep_variance
+            # logging.info(f"Sleeping for {sleep_time} seconds")
+            # time.sleep(sleep_time)
 
     def run_export(self):
         exporter = Exporter()
